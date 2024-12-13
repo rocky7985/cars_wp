481e3dadbd4615a32ef25630df74a1a4cb8bb81e
KANISHK <77665881+rocky7985@users.noreply.github.com>
2024-12-13 19:20:15 +0530

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'cars_db' );
+define( 'DB_NAME', 'railway' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'FvqaUbeppRJxumgoQegRGbNIYDjKxbyV' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'junction.proxy.rlwy.net:38225' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
